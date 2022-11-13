831abc6d45a2f316caecbb825b860628bc4f90fc
VIRGIT <50486221+virgit1@users.noreply.github.com>
2022-11-13 19:43:47 +0100

    Busqueda con substrings

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,6 +1,5 @@
-import {setupRows} from "./rows.js";
-
-export {autocomplete}
+import { setupRows } from "./rows.js";
+export { autocomplete }
 
 function autocomplete(inp, game) {
 
@@ -26,10 +25,23 @@ function autocomplete(inp, game) {
         a.setAttribute("class", "autocomplete-items");
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
+        let lista;
+        let valor;
+        let matches;
+        let parts;
+        let result;
+
         /*for each item in the array...*/
         for (i = 0; i < players.length; i++) {
+            lista = players[i].name.substr(0, players[i].name.length).toUpperCase();
+            valor = val.toUpperCase()
+            matches = match(lista, valor);
+            parts = parse(lista, matches)
+            result = parts.map(
+                part => (part.highlight ? part.text : `<b>${part.text}</b>`)
+            )
             /*check if the item starts with the same letters as the text field value:*/
-            if ( players[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (result.length > 1) {
 
                 b = document.createElement("DIV");
                 b.classList.add('flex', 'items-start', 'gap-x-3', 'leading-tight', 'uppercase', 'text-sm');
@@ -37,7 +49,7 @@ function autocomplete(inp, game) {
 
                 /*make the matching letters bold:*/
                 b.innerHTML += `<div class='self-center'>
-                                    <span class='font-bold'>${players[i].name.substr(0, val.length).toUpperCase()}</span><span class>${players[i].name.substr(val.length).toUpperCase()}</span>
+                                    <span class='font-bold'>${players[i].name.toUpperCase()}</span>
                                     <input type='hidden' name='name' value='${players[i].name}'>
                                     <input type='hidden' name='id' value='${players[i].id}'>
                                 </div>`;
@@ -122,3 +134,143 @@ function autocomplete(inp, game) {
 
 
 
+
+
+//MATCH
+
+//const removeDiacritics = require('remove-accents').remove;
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
+const specialCharsRegex = /[.*+?^${}()|[\]\\]/g;
+
+// http://www.ecma-international.org/ecma-262/5.1/#sec-15.10.2.6
+const wordCharacterRegex = /[a-z0-9_]/i;
+
+const whitespacesRegex = /\s+/;
+
+function escapeRegexCharacters(str) {
+    return str.replace(specialCharsRegex, '\\$&');
+}
+
+function extend(subject, baseObject) {
+    subject = subject || {};
+    Object.keys(subject).forEach((key) => {
+        baseObject[key] = !!subject[key];
+    });
+    return baseObject;
+}
+
+function match(text, query, options) {
+    options = extend(options, {
+        insideWords: false,
+        findAllOccurrences: false,
+        requireMatchAll: false
+    });
+
+    const cleanedTextArray = Array.from(text).map((x) => x.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
+    let cleanedText = cleanedTextArray.join('');
+
+    query = query.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    return (
+        query
+            .trim()
+            .split(whitespacesRegex)
+            // If query is blank, we'll get empty string here, so let's filter it out.
+            .filter((word) => word.length > 0)
+            .reduce((result, word) => {
+                const wordLen = word.length;
+                const prefix =
+                    !options.insideWords && wordCharacterRegex.test(word[0]) ? '\\b' : '';
+                const regex = new RegExp(prefix + escapeRegexCharacters(word), 'i');
+                let occurrence;
+                let index;
+
+                occurrence = regex.exec(cleanedText);
+                if (options.requireMatchAll && occurrence === null) {
+                    cleanedText = '';
+                    return [];
+                }
+
+                while (occurrence) {
+                    index = occurrence.index;
+
+                    const cleanedLength = cleanedTextArray
+                        .slice(index, index + wordLen)
+                        .join('').length;
+                    const offset = wordLen - cleanedLength;
+
+                    const initialOffset =
+                        index - cleanedTextArray.slice(0, index).join('').length;
+
+                    const indexes = [
+                        index + initialOffset,
+                        index + wordLen + initialOffset + offset
+                    ];
+
+                    if (indexes[0] !== indexes[1]) {
+                        result.push(indexes);
+                    }
+
+                    // Replace what we just found with spaces so we don't find it again.
+                    cleanedText =
+                        cleanedText.slice(0, index) +
+                        new Array(wordLen + 1).join(' ') +
+                        cleanedText.slice(index + wordLen);
+
+                    if (!options.findAllOccurrences) {
+                        break;
+                    }
+
+                    occurrence = regex.exec(cleanedText);
+                }
+
+                return result;
+            }, [])
+            .sort((match1, match2) => match1[0] - match2[0])
+    );
+};
+
+//PARSE
+
+function parse(text, matches) {
+    const result = [];
+
+    if (matches.length === 0) {
+        result.push({
+            text,
+            highlight: false
+        });
+    } else if (matches[0][0] > 0) {
+        result.push({
+            text: text.slice(0, matches[0][0]),
+            highlight: false
+        });
+    }
+
+    matches.forEach((match, i) => {
+        const startIndex = match[0];
+        const endIndex = match[1];
+
+        result.push({
+            text: text.slice(startIndex, endIndex),
+            highlight: true
+        });
+
+        if (i === matches.length - 1) {
+            if (endIndex < text.length) {
+                result.push({
+                    text: text.slice(endIndex, text.length),
+                    highlight: false
+                });
+            }
+        } else if (endIndex < matches[i + 1][0]) {
+            result.push({
+                text: text.slice(endIndex, matches[i + 1][0]),
+                highlight: false
+            });
+        }
+    });
+
+    return result;
+};
--- js/stats.js
+++ js/stats.js
@@ -1,60 +1,60 @@
-import {game} from './main.js'
-export {initState, updateStats, getStats, initState, gamestats}
+import { game } from './main.js'
+export { initState, updateStats, getStats, gamestats }
 
 
-let initState = function(what, solutionId) {
+let initState = function (what, solutionId) {
     let WAYgameState = JSON.parse(localStorage.getItem('WAYgameState'))
-    if(WAYgameState == null){
+    if (WAYgameState == null) {
         localStorage.setItem('WAYgameState', JSON.stringify({
-            "guesses" : [],
-            "solution" : solutionId
+            "guesses": [],
+            "solution": solutionId
         }))
         WAYgameState = JSON.parse(localStorage.getItem('WAYgameState'))
     }
-    if(game.guesses.length < 1)
+    if (game.guesses.length < 1)
         WAYgameState.guesses.map(playerID => game.guesses.push(playerID))
 
     let state = WAYgameState.guesses.find(player => player == what)
-    let anonymousFunction = function(guess){
+    let anonymousFunction = function (guess) {
         WAYgameState.guesses.push(guess)
         localStorage.setItem('WAYgameState', JSON.stringify(WAYgameState))
     }
     return [state, anonymousFunction]
 }
 
-function successRate (e){
+function successRate(e) {
     let stats = JSON.parse(localStorage.getItem(e))
     return stats.successRate
 }
 
-let getStats = function(what) {
+let getStats = function (what) {
     let stats = JSON.parse(localStorage.getItem(what))
-    if(stats == null)
-        stats = {   "winDistribution": [],
-                    "gamesFailed": 0,
-                    "currentStreak": 0,
-                    "bestStreak": 0,
-                    "totalGames": 0,
-                    "successRate": 0
-                }
+    if (stats == null)
+        stats = {
+            "winDistribution": [],
+            "gamesFailed": 0,
+            "currentStreak": 0,
+            "bestStreak": 0,
+            "totalGames": 0,
+            "successRate": 0
+        }
     return stats
 };
 
 
-function updateStats(t, won){
+function updateStats(t, won) {
     let gameStats = getStats("gameStats")
-    if(won){
+    if (won) {
         gameStats.winDistribution.push(1)
         gameStats.currentStreak += 1
     }
-    else
-    {
+    else {
         gameStats.winDistribution.push(0)
         gameStats.gamesFailed += 1
         gameStats.currentStreak = 0
     }
 
-    if(gameStats.currentStreak > gameStats.bestStreak)
+    if (gameStats.currentStreak > gameStats.bestStreak)
         gameStats.bestStreak = gameStats.currentStreak
 
     gameStats.totalGames += 1
@@ -66,4 +66,3 @@ function updateStats(t, won){
 
 
 let gamestats = getStats('gameStats');
-
